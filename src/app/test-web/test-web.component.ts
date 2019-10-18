a2c729e04122128bcdda61d9636f4d41c3842453
vlapshov <lapshov@vbankcenter.ru>
2019-10-18 18:08:14 +0300

    prepared a project for building as a web component

--- src/app/test-web/test-web.component.ts
+++ src/app/test-web/test-web.component.ts
@@ -4,15 +4,10 @@ import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angula
   selector: 'app-test-web',
   templateUrl: './test-web.component.html',
   styleUrls: ['./test-web.component.scss'],
-  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class TestWebComponent {
   @Input() title: string;
   @Output() exit = new EventEmitter<void>();
   @Output() collapse = new EventEmitter<void>();
   @Output() expand = new EventEmitter<void>();
-
-
-  constructor() { }
-
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,3 +63,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 import '@webcomponents/custom-elements/src/native-shim';
 import '@webcomponents/custom-elements/custom-elements.min';
+import 'core-js/es/reflect';
